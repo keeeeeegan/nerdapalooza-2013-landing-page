71b9b9a729e9cc1cea7bf63114d98b53a956cdfc
Keegan Berry <keeganberry@gmail.com>
2013-09-02 18:17:51 -0400

    minified marquee.js, adjust the marquee

--- landing-assets/js/marquee.js
+++ landing-assets/js/marquee.js
@@ -2,12 +2,12 @@
 // calculate actual width of marquee
 // http://stackoverflow.com/a/2771544
 $.fn.textWidth = function(){
-  var html_org = $(this).html();
-  var html_calc = '<span>' + html_org + '</span>';
-  $(this).html(html_calc);
-  var width = $(this).find('span:first').width();
-  $(this).html(html_org);
-  return width;
+	var html_org = $(this).html();
+	var html_calc = '<span>' + html_org + '</span>';
+	$(this).html(html_calc);
+	var width = $(this).find('span:first').width();
+	$(this).html(html_org);
+	return width;
 };
 
 // some constants you setup before starting
@@ -17,11 +17,11 @@ var marqueeTime = marqueeWidth * 10;
 // scroll left to the end, then scroll back
 function startMarquee() {
 	$('.marquee').animate({
-		scrollLeft: marqueeWidth + 25
+		scrollLeft: marqueeWidth
 		}, marqueeTime, "linear", function() {
 			//startMarquee();
 			$(this).animate({
-				scrollLeft: -25
+				scrollLeft: 0
 				}, (marqueeTime), "linear", function() {
 					startMarquee();
 			});
