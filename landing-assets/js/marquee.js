276e5b5141a7daf85ef6a286f898b1010899fe53
Keegan Berry <keeganberry@gmail.com>
2013-09-02 15:35:05 -0400

    initial commit

---
+++ landing-assets/js/marquee.js
@@ -0,0 +1,31 @@
+
+// calculate actual width of marquee
+// http://stackoverflow.com/a/2771544
+$.fn.textWidth = function(){
+  var html_org = $(this).html();
+  var html_calc = '<span>' + html_org + '</span>';
+  $(this).html(html_calc);
+  var width = $(this).find('span:first').width();
+  $(this).html(html_org);
+  return width;
+};
+
+// some constants you setup before starting
+var marqueeWidth = $('.marquee').textWidth();
+var marqueeTime = marqueeWidth * 10;
+
+// scroll left to the end, then scroll back
+function startMarquee() {
+	$('.marquee').animate({
+		scrollLeft: marqueeWidth + 25
+		}, marqueeTime, "linear", function() {
+			//startMarquee();
+			$(this).animate({
+				scrollLeft: -25
+				}, (marqueeTime), "linear", function() {
+					startMarquee();
+			});
+	});
+}
+
+startMarquee();
