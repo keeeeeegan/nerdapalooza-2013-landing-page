ad2c72d64323e1a94afd811c0c0307afeb92ef2c
Keegan Berry <keeganberry@gmail.com>
2013-10-09 01:14:45 -0400

    removed extra js files, changed carousel to check for mobile

--- landing-assets/js/carousel.js
+++ landing-assets/js/carousel.js
@@ -1,14 +1,17 @@
 // super simple image switcher for nerdapalooza page
 
-var count = 1;
-var next = 0;
-var max = $('.carousel .photo').length;
+// quickly checking to see if we're on a mobile device using some css
+if ($('body').css('overflow-x') != 'hidden') {
+	var count = 1;
+	var next = 0;
+	var max = $('.carousel .photo').length;
 
-// every 5000 ms change the image (fade it all fancy with the next one)
-setInterval(function() {
-	$('.carousel .photo:nth-child(' + count + ')').fadeOut('slow');
-	// use the ternary operator to determine the next image or loop to the first
-	next = (count + 1 <= max) ? (count + 1) : 1;
-	$('.carousel .photo:nth-child(' + next + ')').fadeIn('slow');
-	count = next;
-}, 5000);
+	// every 5000 ms change the image (fade it all fancy with the next one)
+	setInterval(function() {
+		$('.carousel .photo:nth-child(' + count + ')').fadeOut('slow');
+		// use the ternary operator to determine the next image or loop to the first
+		next = (count + 1 <= max) ? (count + 1) : 1;
+		$('.carousel .photo:nth-child(' + next + ')').fadeIn('slow');
+		count = next;
+	}, 5000);
+}
